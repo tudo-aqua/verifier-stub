1f111ce3b418797dd96ecbf88c7edb95509124aa
Malte Mues <mail.mues@gmail.com>
2024-01-15 08:57:12 +0100

    Making randomness more controlled

--- src/main/java/tools/aqua/concolic/RandomVerifierInput.java
+++ src/main/java/tools/aqua/concolic/RandomVerifierInput.java
@@ -5,9 +5,14 @@
  */
 
 package tools.aqua.concolic;
+import java.util.ArrayList;
 import java.util.Random;
 
 public final class RandomVerifierInput {
+  private static int[] preSeededInts = {0,2,4,8,16,-2,-4,-8,-16};
+  private static float[] preSeededFloats = {0f,2f,4f,8f,16f,-2f,-4f,-8f,-16f};
+  private static ArrayList<Integer> oldIntVals = new ArrayList();
+  private static ArrayList<Float> oldFloatVals = new ArrayList();
   private static Random r = new Random();
 
   public static void assume(boolean condition) {}
@@ -17,31 +22,31 @@ public final class RandomVerifierInput {
   }
 
   public static byte nondetByte() {
-    return (byte )r.nextInt();
+    return (byte )generateInt();
   }
 
   public static char nondetChar() {
-    return (char) r.nextInt();
+    return (char) generateInt();
   }
 
   public static short nondetShort() {
-    return (short) r.nextInt();
+    return (short) generateInt();
   }
 
   public static int nondetInt() {
-    return r.nextInt();
+    return generateInt();
   }
 
   public static long nondetLong() {
-    return r.nextLong();
+    return generateInt();
   }
 
   public static float nondetFloat() {
-    return r.nextFloat();
+    return generateFloat();
   }
 
   public static double nondetDouble() {
-    return r.nextDouble();
+    return generateFloat();
   }
 
   public static String nondetString() {
@@ -51,4 +56,43 @@ public final class RandomVerifierInput {
     return new String(b);
   }
 
+  private static int generateInt(){
+      int random = -2000;
+      double chance = r.nextGaussian();
+      if (chance < -1){
+        int index = r.nextInt(preSeededInts.length - 1);
+        random = preSeededInts[index];
+      } else if(chance > 1 && oldIntVals.size() > 0){
+        int index = r.nextInt(oldIntVals.size());
+        random = oldIntVals.get(index);
+      }else{
+        random = r.nextInt(1000);
+        if (r.nextBoolean()){
+          random = random*-1;
+        }
+        oldIntVals.add(random);
+      }
+      System.out.println("Generated: " + random+ " by chance: "+ chance);
+      return random;
+  }
+
+  private static float generateFloat(){
+    float random = -2000f;
+    double chance = r.nextGaussian();
+    if (chance < -1){
+      int index = r.nextInt(preSeededInts.length);
+      random = preSeededInts[index];
+    } else if(chance > 1 && oldFloatVals.size() > 0 ){
+      int index = r.nextInt(oldFloatVals.size());
+      random = oldFloatVals.get(index);
+    }else{
+      random = r.nextFloat() * 1000;
+      if (r.nextBoolean()){
+        random = random*-1;
+      }
+      oldFloatVals.add(random);
+    }
+    System.out.println("Generated: " + random+ " by chance: "+ chance);
+    return random;
+  }
 }

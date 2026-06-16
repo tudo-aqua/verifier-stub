5a11ade7636fc1194649293b6baf782a96fcd61f
Usman Khawar <khawar@uni-wuppertal.de>
2026-06-16 11:54:14 +0200

    path traversal taint added

--- src/main/java/tools/aqua/concolic/TaintIdResolver.java
+++ src/main/java/tools/aqua/concolic/TaintIdResolver.java
@@ -7,20 +7,39 @@ import static tools.aqua.concolic.Tainting.WEAK_HASHING_CONFIG;
 import static tools.aqua.concolic.Tainting.WEAK_CRYPTO_CONFIG;
 import static tools.aqua.concolic.Tainting.XPATH_INJECTION;
 import static tools.aqua.concolic.Tainting.LDAP_INJECTION;
+import static tools.aqua.concolic.Tainting.PATH_TRAVERSAL;
 
 public class TaintIdResolver {
 
-	public static void main(String args[]){
-		String taintId = args[0];
-		switch(Integer.parseInt(taintId)){
-		case XSS: System.out.println("XSS"); break;
-		case SQL_INJECTION: System.out.println("SQL_INJECTION");break;
-		case CMD_INJECTION: System.out.println("CMD_INJECTION");break;
-		case WEAK_HASHING_CONFIG: System.out.println("WEAK_HASHING_CONFIG");break;
-		case WEAK_CRYPTO_CONFIG: System.out.println("WEAK_CRYPTO_CONFIG");break;
-        case XPATH_INJECTION: System.out.println("XPATH_INJECTION_TAINT");break;
-        case LDAP_INJECTION: System.out.println("LDAP_INJECTION_TAINT");break;
-		default: System.out.println("unkown taint id: {}".formatted(taintId));
-		}
-	}
+    public static void main(String args[]) {
+        String taintId = args[0];
+        switch (Integer.parseInt(taintId)) {
+            case XSS:
+                System.out.println("XSS");
+                break;
+            case SQL_INJECTION:
+                System.out.println("SQL_INJECTION");
+                break;
+            case CMD_INJECTION:
+                System.out.println("CMD_INJECTION");
+                break;
+            case WEAK_HASHING_CONFIG:
+                System.out.println("WEAK_HASHING_CONFIG");
+                break;
+            case WEAK_CRYPTO_CONFIG:
+                System.out.println("WEAK_CRYPTO_CONFIG");
+                break;
+            case XPATH_INJECTION:
+                System.out.println("XPATH_INJECTION_TAINT");
+                break;
+            case LDAP_INJECTION:
+                System.out.println("LDAP_INJECTION_TAINT");
+                break;
+            case PATH_TRAVERSAL:
+                System.out.println("PATH_TRAVERSAL_TAINT");
+                break;
+            default:
+                System.out.println("unkown taint id: {}".formatted(taintId));
+        }
+    }
 }
